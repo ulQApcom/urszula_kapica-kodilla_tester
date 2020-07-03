a5da44e5e35903cf56704abbe02a689c283ed26a
Urszula Kapica <urszula.kapica@gmail.com>
2020-07-03 11:26:02 +0200

    add new files

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Urszula");
+        System.out.println(37);
+        System.out.println('K');
+    }
+}
