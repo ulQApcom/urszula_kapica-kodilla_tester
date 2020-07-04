74712f6696434d41d8a03392ea6579daafc34f04
Urszula Kapica <urszula.kapica@gmail.com>
2020-07-04 23:12:38 +0200

    Add new files

--- src/main/java/FirstClass.java
+++ src/main/java/FirstClass.java
@@ -1,5 +1,6 @@
-public class FirstClass {
-    public static void main(String[] args) {
-        System.out.println("Hello from FirstClass!");
+public class FirstClass{
+    public static void main(String[]args){
+        String example = "Hello from FirstClass";
+        System.out.println("example");
     }
 }
---
+++ src/main/java/MainExtended.java
@@ -0,0 +1,10 @@
+public class MainExtended {
+    public static void main (String[]args){
+        String myKidsName  = "Cela i Antek";
+        int celaAge = 6;
+        int antekAge = 4;
+        boolean isGoodKids = true;
+        char quantityBruise = '9';
+        double celaHeight = 1.40;
+    }
+}
